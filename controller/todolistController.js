f8ed03ef9450514eeaf13354dc023f77b80f1a8d
Daniel YL YIP <2602505y@student.gla.ac.uk>
2022-04-04 15:36:04 +0100

    fix refresh

--- controller/todolistController.js
+++ controller/todolistController.js
@@ -3,9 +3,11 @@ import itemModel from '../models/todolistItem.js';
 
 export const gettodoList = async (req, res) => {
     try{
-        console.log('fetched')
+        // console.log('start fetch')
         const items = await itemModel.find()
+        
         res.json(items)
+        // console.log('fetched')
         // res.send({message: 'working'})
     }catch(err){
         console.log(err);
@@ -18,6 +20,46 @@ export const createNewItem = async(req, res) =>{
         const newItem = req.body
         const item = new itemModel(newItem)
         item.save()
+        // console.log('added')
+        res.json(item)
+    }catch(err){
+        console.log(err);
+        res.json({message: err})
+    }
+}
+
+export const getAItem = async(id) =>{
+    try{
+        if (!mongoose.isValidObjectId(id)) return ('cannot find the post')
+        let item = await itemModel.findById(id)
+        return item
+        // console.log(item)
+    }catch(err){
+        console.log(err);
+        return (err)
+    }
+}
+
+export const updateChecked = async(req, res) =>{
+    try{
+        const {id}=req.params
+        const item = await getAItem(id)
+        item.finished = !item.finished
+        item.save()
+        // console.log(item)
+        res.json(item)
+    }catch(err){
+        console.log(err);
+        res.json({message: err})
+    }
+}
+
+export const deleteItem = async(req, res) =>{
+    try{
+        const {id}=req.params
+        if (!mongoose.isValidObjectId(id)) return res.status(404).json({message: 'cannot find the post'})
+        await itemModel.findByIdAndDelete(id)
+        res.json({message: 'Deleted item'})
     }catch(err){
         console.log(err);
         res.json({message: err})
--- router/todolist.js
+++ router/todolist.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {gettodoList, createNewItem} from '../controller/todolistController.js'
+import {gettodoList, createNewItem, getAItem, deleteItem, updateChecked} from '../controller/todolistController.js'
 
 const router = express.Router()
 
 router.get('/', gettodoList)
 router.post('/', createNewItem)
+router.get('/:id/', getAItem)
+router.delete('/:id', deleteItem)
+router.patch('/:id/checked', updateChecked)
 
 export default router
